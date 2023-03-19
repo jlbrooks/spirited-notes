69d9d0e9f2c910e53ffa8d944f28caf688d56b27
Jacob Brooks <jbrooks2@godaddy.com>
2023-03-19 09:52:45 -0700

    Initial Commit

---
+++ Spirited Notes/Spirited_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spirited_NotesApp.swift
+//  Spirited Notes
+//
+//  Created by Jacob Brooks on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Spirited_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
